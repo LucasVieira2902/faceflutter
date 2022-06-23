5339890d2eab93d2b3bcb50afe0f0fb51d772f89
Lucas Vieira <66380573+LucasVieira2902@users.noreply.github.com>
2022-06-23 18:49:50 -0300

    Adicionado Appbar

---
+++ lib/telas/home.dart
@@ -0,0 +1,55 @@
+import 'package:faceflutter/components/circular_button.dart';
+import 'package:faceflutter/utils/paleta_cores.dart';
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icon.dart';
+import 'package:line_icons/line_icons.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            // expandedHeight: 250,
+            floating: true,
+            title: Text(
+              "facebook",
+              style: TextStyle(
+                color: PaletaCores.azulFacebook,
+                fontWeight: FontWeight.bold,
+                fontSize: 28,
+                letterSpacing: -1.2,
+              ),
+            ),
+            actions: [
+              CircularButton(
+                icon: LineIcons.search, 
+                iconSize: 30, 
+                onPressed: (){}
+              ),
+              CircularButton(
+                icon: LineIcons.facebookMessenger, 
+                iconSize: 30, 
+                onPressed: (){}
+              ),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 10000,
+              color: Colors.green,
+            )
+          ),
+      ]),
+    );
+  }
+}
